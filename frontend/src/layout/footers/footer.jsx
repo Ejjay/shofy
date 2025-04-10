9e1c49e69d7190138b0c0078026bb2b53b88ab8f
ejjays <christsonalloso021@gmail.com>
2025-04-10 14:03:12 +0800

    Auto commit

--- frontend/src/layout/footers/footer.jsx
+++ frontend/src/layout/footers/footer.jsx
@@ -23,7 +23,7 @@ const Footer = ({ style_2 = false, style_3 = false,primary_style=false }) => {
                         <Image src={logo} alt="logo" />
                       </Link>
                     </div>
-                    <p className="tp-footer-desc">We are a dynamic team of full stack developers and designers crafting high-quality web applications</p>
+                    <p className="tp-footer-desc">We are a dynamic running online shop, dedicated to offering high-quality products and a seamless shopping experience.</p>
                     <div className="tp-footer-social">
                       {social_data.map(s => <a href={s.link} key={s.id} target="_blank">
                         <i className={s.icon}></i>
